7325fead1bbac0488cdfb2d1e61064a322bdc0ff
Karmel <32240709+KSZLAGK@users.noreply.github.com>
2021-01-19 05:06:23 +0100

    Update CommentsKarmelEmojis.js

--- CommentsKarmelEmojis.js
+++ CommentsKarmelEmojis.js
@@ -18,7 +18,7 @@ export function main()
     // -- Emoji list --
     if (h1.innerHTML.indexOf("!emotes") !== -1)
     {
-    h1.innerHTML = h1.innerHTML.replace(/!emotes/g, "<a style=\"color: #00b0f4;\" href=\"h\">Lista_dostępnych_emotek<\/a>");
+    h1.innerHTML = h1.innerHTML.replace(/!emotes/g, "<a style=\"color: #00b0f4;\" href=\"https://kszlagk.github.io/site/CommentsKarmelEmotesList.html\">Lista dostępnych emotek<\/a>");
     }
     if(h1.innerHTML.indexOf(":rickroll:") !== -1) 
     {
