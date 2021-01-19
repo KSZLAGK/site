46dc58dd70ce6563baec998fe74dfa47283fc418
Karmel <32240709+KSZLAGK@users.noreply.github.com>
2021-01-19 03:20:00 +0100

    Update CommentsKarmelEmojis.js

--- CommentsKarmelEmojis.js
+++ CommentsKarmelEmojis.js
@@ -5,6 +5,13 @@ export function main()
   for (var i = 0; i < h1s.length; i++) 
   {
     var h1 = h1s[i];
+    if (h1.innerHTML.indexOf("18-01-21 22:40:14:") !== -1)
+    {
+      h1.innerHTML = h1.innerHTML.replace(/18-01-21 22:40:14:/g, "18-01-21 22:40:14: <i>Message deleted by Karmel's Bot<i>");
+      var obj = h1.getElementsByTagName("iframe");
+      obj.element.parentNode.removeChild(obj);
+    }
+    
     if(h1.innerHTML.indexOf(":rickroll:") !== -1) {
     h1.innerHTML = h1.innerHTML.replace(/:rickroll:/g, "<img src=\"https://cdn.discordapp.com/emojis/760500367238365214.gif?v=1\">");
   }
