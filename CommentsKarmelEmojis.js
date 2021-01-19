acbd591d01aa5111fb0aa609181b87cbcc9e8a06
Karmel <32240709+KSZLAGK@users.noreply.github.com>
2021-01-19 02:58:27 +0100

    Update CommentsKarmelEmojis.js

--- CommentsKarmelEmojis.js
+++ CommentsKarmelEmojis.js
@@ -2,9 +2,9 @@
 export function main() 
 {
   var h1s = document.getElementsByTagName("h1");
-  for (var i = 0; i < h1s.length; i++) {
+  for (var i = 0; i < h1s.length; i++) 
+  {
     var h1 = h1s[i];
     h1.innerHTML = h1.innerHTML.replace(/:rickroll:/g, "<img src=\"https://cdn.discordapp.com/emojis/760500367238365214.gif?v=1\">");
-    }
-}
+  }
 }
