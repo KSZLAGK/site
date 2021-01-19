ddb04031bcd955c45e01eb054a7aa123c11450bb
Karmel <32240709+KSZLAGK@users.noreply.github.com>
2021-01-19 04:09:07 +0100

    Update CommentsKarmelEmojis.js

--- CommentsKarmelEmojis.js
+++ CommentsKarmelEmojis.js
@@ -26,7 +26,7 @@ export function main()
   if (inputForm.getAttribute("style") !== "font-size:30px;position:fixed;top:20px;")
   {
     document.getElementById('bottomOfMessages').scrollIntoView();
-  inputForm.setAttribute("style", "font-size:30pxposition:fixed;top:20px;");
+  inputForm.setAttribute("style", "font-size:30px;position:fixed;top:20px;");
   document.getElementById('bottomOfMessages').scrollIntoView();
   }
     
