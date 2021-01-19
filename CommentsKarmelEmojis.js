cdf2f632790329cdddcbbb86681ee89498b778b7
Karmel <32240709+KSZLAGK@users.noreply.github.com>
2021-01-19 02:54:05 +0100

    Update CommentsKarmelEmojis.js

--- CommentsKarmelEmojis.js
+++ CommentsKarmelEmojis.js
@@ -1,4 +1,5 @@
+
 export function main() 
 {
-  document.body.innerHTML = document.body.innerHTML.replace(/:rickroll:/g, "<img src=\"https://cdn.discordapp.com/emojis/760500367238365214.gif?v=1\">");
+  document.getElementsByTagName('h1').innerHTML = document.getElementsByTagName('h1').replace(/:rickroll:/g, "<img src=\"https://cdn.discordapp.com/emojis/760500367238365214.gif?v=1\">");
 }
