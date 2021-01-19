9d6364b8692ad868bd78590bb10a8579a77a385c
Karmel <32240709+KSZLAGK@users.noreply.github.com>
2021-01-19 04:31:26 +0100

    Update CommentsKarmelEmojis.js

--- CommentsKarmelEmojis.js
+++ CommentsKarmelEmojis.js
@@ -25,7 +25,6 @@ export function main()
   var inputForm = document.getElementById("inputForm");
   if (inputForm.getAttribute("style") !== "background-color:#36393f;font-size:30px;position:fixed;left:0.1%;right: 0.1%;top:0.01%")
   {
-    document.getElementById('bottomOfMessages').scrollIntoView();
   inputForm.setAttribute("style", "background-color:#36393f;font-size:30px;position:fixed;left:0.1%;right: 0.1%;top:0.01%");
   document.getElementById('bottomOfMessages').scrollIntoView();
   }
